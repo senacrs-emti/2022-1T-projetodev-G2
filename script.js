ced3f68b3a3b47cfee50e6de9dc21dd3e6f37a69
Felipe Martins Yung <102309048+FelipeMYung@users.noreply.github.com>
2022-10-27 12:08:10 -0300

    Base do jogador

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+import Jogador from "./Jogador";
 //criar a variavel do jogo e pedir para o canvas trabalhar em 2d
+
 const canvas = document.getElementById('jogo');
 const ctx = canvas.getContext('2d');
-
+const Jogador = new Jogador()
 let velocidade = 4;
 
 //game loop
