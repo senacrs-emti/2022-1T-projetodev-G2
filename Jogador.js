0a29213f87d6d97264e63e9243a9f014938794e3
Roberto M. Leitão <93278319+betoleitao5@users.noreply.github.com>
2022-10-27 21:03:36 -0300

    V1
    +FINALMENTE ALGO
    +personagem

--- Jogador.js
+++ Jogador.js
@@ -1,16 +1,16 @@
-export default class Jogador{
-    constructor(x, y){
+export default class Player{
+    constructor(x,y){
         this.x = x;
         this.y = y;
-        this.whidth = 50;
+        this.width = 50;
         this.height = 50;
-        this.velocidade = 4;
+        this.speed = 4;
     }
-
     draw(ctx){
-        ctx.strokeStyle= "purple";
-        ctx.strokeRect(this.x, this.y, this.whidth, this.height);
-        ctx.fillStyle = "black";
-        ctx.fillRect(this.x, this.y, this.whidth, this.height);
+        ctx.strokeStyle = 'yellow';
+        ctx.strokeRect(this.x,this.y, this.width, this.height)
+        ctx.fillStyle = 'black';
+        ctx.fillRect(this.x,this.y, this.width, this.height)
     }
 }
+
--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-import Jogador from "./Jogador";
+import Player from "./Jogador.js";
 //criar a variavel do jogo e pedir para o canvas trabalhar em 2d
 
 const canvas = document.getElementById('jogo');
 const ctx = canvas.getContext('2d');
-const Jogador = new Jogador()
-let velocidade = 4;
+
+const player = new Player(canvas.width/2.2, canvas.height/1.3)
+
+//tela de desenho
+function clearScream(){
+  ctx.fillStyle = 'black' ;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
 
 //game loop
 function drawGame() {
   clearScream();
-  setTimeout(drawGame, 1000 / velocidade);
+  setTimeout(drawGame, 10000/ 60);
+  player.draw(ctx);
 };
-//tela de desenho
-function clearScream(){
-  ctx.fillStyle = 'black' ;
-  ctx.fillRect(0,0,canvas.width,canvas.height);
-}
 
-drawGame()
+drawGame();
