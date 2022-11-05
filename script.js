8800faae39126462437cd8d92202fe2f8fd8a9f6
Felipe Martins Yung <102309048+FelipeMYung@users.noreply.github.com>
2022-11-05 11:01:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,22 +16,11 @@ function clearScream(){
   ctx.fillStyle = 'black' ;
   ctx.fillRect(0,0,canvas.width,canvas.height);
 }
-funcition Jogador{
-  constructor(x, y){
-    this.x = x;
-    this.y = y;
-    this.whidth = 50;
-    this.height = 50;
-    this.velocidade = 4;
-}
 
-draw(ctx){
-    ctx.strokeStyle= "purple";
+draw(ctx)
+    ctx.strokeStyle= "red";
     ctx.strokeRect(this.x, this.y, this.whidth, this.height);
-    ctx.fillStyle = "black";
+    ctx.fillStyle = "purple";
     ctx.fillRect(this.x, this.y, this.whidth, this.height);
-}
-}
-}
 
 drawGame()
