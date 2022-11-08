00d31c2f8372f5062de7cf9f722a40f657f4c535
FelipeMYung <102309048+FelipeMYung@users.noreply.github.com>
2022-11-08 17:29:17 -0300

    Fundo
    Fiz o fundo mas ta grande, tem que diminuir

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-import Player from "./Jogador.js";
+
 //criar a variavel do jogo e pedir para o canvas trabalhar em 2d
 
 const canvas = document.getElementById('jogo');
 const ctx = canvas.getContext('2d');
 
-const player = new Player(canvas.width/2.2, canvas.height/1.3)
+var fundoimg = new Image()
+
+function fundo(){
+  fundoimg.src = "./img/Cenário1.png";
+  ctx.drawImage(fundoimg, 0, 0);  
+}
 
 //tela de desenho
 function clearScream(){
   ctx.fillStyle = 'black' ;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+  fundo();
 }
-
 //game loop
 function drawGame() {
   clearScream();
   setTimeout(drawGame, 10000/ 60);
-  player.draw(ctx);
+  ctx.drawImage(fundoimg, 0, 0)
 };
 drawGame();
