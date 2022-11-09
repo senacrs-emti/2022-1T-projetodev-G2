8290a9b4deee4046b8f09e190bd0befc4ec10c6a
Felipe Martins Yung <102309048+FelipeMYung@users.noreply.github.com>
2022-11-09 11:16:59 -0300

    atualização

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 
 //criar a variavel do jogo e pedir para o canvas trabalhar em 2d
 
-const canvas = document.getElementById('jogo');
-const ctx = canvas.getContext('2d');
+var canvas = document.getElementById('jogo');
+var ctx = canvas.getContext('2d');
 
 var fundoimg = new Image()
+var personagem = new Image();//Imagem que será carregada e desenhada na canvas
+var posicao = 0;//Indicador da posição atual do personagem
+var NUM_POSICOES = 6;//Quantidade de imagens que compõem o movimento
 
 function fundo(){
   fundoimg.src = "./img/Cenário.png";
   ctx.drawImage(fundoimg, 0, 0);  
 }
-
 //tela de desenho
 function clearScream(){
   ctx.fillStyle = 'black' ;
@@ -21,6 +23,6 @@ function clearScream(){
 function drawGame() {
   clearScream();
   setTimeout(drawGame, 10000/ 60);
-  ctx.drawImage(fundoimg, 0, 0)
+  ctx.drawImage(fundoimg, 0, 0);
 };
 drawGame();
