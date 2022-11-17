9d65c15bf1df2a3cde6096980d3c540bf9eafe83
Felipe Martins Yung <102309048+FelipeMYung@users.noreply.github.com>
2022-11-17 10:50:01 -0300

    ajustando o personagem no canvas

--- script.js
+++ script.js
@@ -6,25 +6,16 @@ var ctx = canvas.getContext('2d');
 
 var fundoimg = new Image()
 var personagem = new Image();//Imagem que será carregada e desenhada na canvas
-var posicao = 0;//Indicador da posição atual do personagem
-var NUM_POSICOES = 6;//Quantidade de imagens que compõem o movimento
 var gravidade = 1.5;
-var chão = 60;
 
 function fundo(){
   fundoimg.src = "./img/Cenário.png";
   ctx.drawImage(fundoimg, 0, 0);  
 }
-//tela de desenho
-function clearScream(){
-  ctx.fillStyle = 'black' ;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  fundo();
-}
 class Jogador{
   constructor(){
-    this.position = {
-      x:100,
+    this.posicao = {
+      x:100,  
       y:100
     }
     this.velocidade = {
@@ -35,42 +26,30 @@ class Jogador{
     this.height = 30
   }
   desenhar(){
-    ctx.fillStyle = 'red'
-    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    ctx.fillRect(this.posicao.x, this.posicao.y, this.width, this.height)
+    ctx.fillStyle = ('red')
   }
   update(){
     this.desenhar()
-    this.position.y += this.velocidade.y
+    this.posicao.y += this.velocidade.y
 
-    if (this.position.y + this.height + this.velocidade.y <= canvas.height)
+    if (this.posicao.y + this.height + this.velocidade.y <= 550)
     this.velocidade.y += gravidade
     else this.velocidade.y = 0
     }
 }
 const jogador = new Jogador()
+
 //game loop
-function drawGame() {
-  clearScream();
-  setTimeout(drawGame, 10000/ 60);
-  ctx.drawImage(fundoimg, 0, 0);
-  requestAnimationFrame(drawGame)
+function animate(){
+  requestAnimationFrame(animate)  
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  fundo();
   jogador.update()
-
-};
-drawGame();
-
-function KeyDown(evt){
-    switch (evt.keyCode) {
-        case 39:  /*seta para direita*/
-            if (x + velocidade < canvas.width){
-                x += velocidade;
-                posicao++;
-                if(posicao == NUM_POSICOES)
-                    posicao = 1;
-            }
-            break;            
-    }
 }
+
+animate()
+
 addEventListener('keydown', ({ keyCode }) => {
     switch (keyCode) {
       case 65:
